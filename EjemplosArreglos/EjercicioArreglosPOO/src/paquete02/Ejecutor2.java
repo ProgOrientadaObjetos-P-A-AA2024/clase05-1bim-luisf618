3da0a174627f70db2490dad02db4688b17e85f38
Luis Cordova <lcordovacarrion@gmail.com>
2024-05-15 15:59:21 -0500

    Clase

--- EjemplosArreglos/EjercicioArreglosPOO/src/paquete02/Ejecutor2.java
+++ EjemplosArreglos/EjercicioArreglosPOO/src/paquete02/Ejecutor2.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package paquete02;
 
@@ -9,7 +8,7 @@ import paquete01.LibretaCalificacion;
 
 /**
  *
- * @author reroes
+ * @author utpl
  */
 public class Ejecutor2 {
 
@@ -24,4 +23,5 @@ public class Ejecutor2 {
         libreta.establecerPromedioCualitativo();
         System.out.printf("\n%s\n", libreta);
     }
+
 }
